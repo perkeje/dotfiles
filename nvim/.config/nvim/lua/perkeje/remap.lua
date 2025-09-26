85652aa901d947a3be7f4ab697bab2dc1b4c04c4
Juraj Peric <juraj.peric@gmail.com>
2025-09-26 12:17:55 +0200

    Update

--- nvim/.config/nvim/lua/perkeje/remap.lua
+++ nvim/.config/nvim/lua/perkeje/remap.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
